7ff039a801d8531ab78451dac922d5014db2362b
DeLaCruz26 <dametrik.fick@gmail.com>
2021-02-20 21:45:04 -0500

    react todo app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
 import './App.css';
+import Form from './components/form'
+import List from './components/list'
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  useEffect(() => {
+    const filterHandler = () => {
+      switch(status){
+        case 'completed':
+          setFilteredTodos(todos.filter(todo => todo.completed === true))
+          break
+        case 'uncompleted':
+          setFilteredTodos(todos.filter(todo => todo.completed === false))
+          break
+        default:
+          setFilteredTodos(todos)
+          break
+      }
+    }
+    filterHandler()
+  }, [todos, status]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>The List</h1>
+        <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText} setStatus={setStatus}  />
+        <List setTodos={setTodos} todos={todos} filteredTodos={filteredTodos} />
       </header>
     </div>
   );
